fc08f0f384ad35326eb8281add2ce123d1b4ede8
sgulhane02 <sgulhane02>
2023-07-23 15:32:54 +0530

    This is first project of retail location service

---
+++ src/main/java/com/example/ReatailLocation/ReatailLocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReatailLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReatailLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReatailLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReatailLocation/ReatailLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReatailLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReatailLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
